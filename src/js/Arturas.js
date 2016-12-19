93df18a123dd8799cfada4e565db002460e0bdce
Artės <artraguckas@gmail.com>
2016-12-19 20:01:21 +0200

    css mergink sigi

---
+++ src/js/Arturas.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Artės on 2016-12-16.
+ */
+angular.module('app', []).controller('ArturasRaguckas', function () {
+    var ar = this;
+    ar.test = "Arturas";
+    ar.vardas = "Raguckas";
+    ar.par1 = true;
+ar.masyvas = ['vienas','du','trys','keturi', '...'];
+})
