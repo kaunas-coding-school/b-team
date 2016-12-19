cafb6d43d27717e4fc337a14366bb9a219afd04f
snagas <vaidas.trade@gmail.com>
2016-12-19 19:53:43 +0200

    20161219

---
+++ src/js/Vaidas.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Lenovo T550 on 16/12/2016.
+ */
+
+angular.module('App', []).controller ('VaidasSurblys', function() {
+var vs = this;
+    vs.kintamasis = 'Labas';
+    vs.Vardas = "Jonas";
+    vs.Pavarde = "Jonaitis";
+    vs.par1=true;
+
+    vs.masyvas = ['Vienas', 'Du', 'Trys', 'Keturi', '...' ];
+
+});
+
+
