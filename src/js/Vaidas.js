3d16cd4eb70417219b98428bfe67d6a5f27ad580
snagas <vaidas.trade@gmail.com>
2016-12-20 19:26:00 +0200

    20161220

--- src/js/Vaidas.js
+++ src/js/Vaidas.js
@@ -2,7 +2,21 @@
  * Created by Lenovo T550 on 16/12/2016.
  */
 
-angular.module('App', []).controller ('VaidasSurblys', function() {
+angular.module('App', ["ngRoute"]).config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "Arturas.html"
+        })
+        .when("/red", {
+            templateUrl : "Ignas.htm"
+        })
+        .when("/green", {
+            templateUrl : "povilas.zilionis.htm"
+        })
+        .when("/blue", {
+            templateUrl : "Soccer.htm"
+        });
+}).controller ('VaidasSurblys', function() {
 var vs = this;
     vs.kintamasis = 'Labas';
     vs.Vardas = "Jonas";
