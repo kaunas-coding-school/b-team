89566de283468765b2304145b480cd0ab501f629
IgnasBaltokas <ignas.baltokas@gmail.com>
2016-12-20 19:37:37 +0200

    Comitinu

---
+++ src/js/controllers/Ignas.controller.js
@@ -0,0 +1,20 @@
+/**
+ * Created by ignas on 16/12/2016.
+ */
+
+
+app.controller('IgnasBaltokas', function () {
+
+
+
+
+
+    var ib = this;
+     ib.kintamasis = 'jaunuoliai'
+    ib.vardas = 'Ignas'
+    ib.par1=true;
+     ib.masyvas = ['vienas','du','trys','keturi'];
+
+
+
+});
